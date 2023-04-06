2f602eb49d19fd0d065b242c6b76ddad3ef3d00a
Matt Waelder <mattwaelder@gmail.com>
2023-04-05 17:48:24 -0700

    all buttons stored are stored in an array and now displayed as template literals on dom

---
+++ src/calc/button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ btn }) => {
+  return <div>{`${btn}`}</div>;
+};
+
+export default Button;
--- src/calc/buttons.jsx
+++ src/calc/buttons.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Button from "./button.jsx";
 
 const ButtonsPanel = () => {
-  return <h1>buttons</h1>;
+  const buttonList = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    ".",
+    "+",
+    "-",
+    "x",
+    "/",
+    "^",
+    "del",
+    "c",
+  ];
+
+  return (
+    <div>
+      <h1>buttons</h1>
+      {buttonList.map((button, i) => <Button btn={button} />) || null}
+    </div>
+  );
 };
 
 export default ButtonsPanel;
